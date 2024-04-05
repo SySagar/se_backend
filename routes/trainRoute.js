acf835740f9710c2a12e1419e177815435a0d8d3
SySagar <sysagar07@gmail.com>
2024-04-05 22:13:02 +0530

    changes

--- routes/trainRoute.js
+++ routes/trainRoute.js
@@ -1,23 +1,39 @@
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
-import rail from 'indian-rail-api';
+// import rail from 'indian-rail-api';
+import axios from 'axios';
 
 
 const router = express.Router();
 
-router.get('/search', expressAsyncHandler(async (req, res) => {
-    const { from, to, date } = req.query;
+router.post('/search', (async (req, res) => {
+    const { from, to, date } = req.body;
     console.log(from, to, date);
     console.log('searching train...',req.query)
     // format date in dd-mm-yyyy
     const newDate = date.split('/').reverse().join('-');
-    rail.getTrainOnDate("ST", "ANND" , 7-11-2022 , (r) => {
-        const json = JSON.parse(r);
+    // rail.getTrainOnDate("ST", "ANND" , 7-11-2022 , (r) => {
+    //     const json = JSON.parse(r);
 
-        // console log with depth
-        console.log(json);
-        res.status(200).json(json);
-    });
+    //     // console log with depth
+    //     console.log(json);
+    //     res.status(200).json(json);
+    // });
+
+    const response = await axios.post('https://se-rail-api.onrender.com/searchTrain', {
+        "from": from,
+        "to": to,
+        "date": newDate
+    }, {
+        headers: {
+            'content-type': 'application/json',
+        }
+        });
+        console.log(response.data);
+        res.status(200).json(
+            response.data
+        );
+
 }));
 
 export default router;
